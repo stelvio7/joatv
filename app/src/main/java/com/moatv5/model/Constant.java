2e66643049e21fcd95f710ee3cf00ea982c8511e
stelvio7 <oreilly7@naver.com>
2017-05-30 22:27:58 +0900

    0530

---
+++ app/src/main/java/com/moatv5/model/Constant.java
@@ -0,0 +1,7 @@
+package com.moatv5.model;
+
+public class Constant {
+	public static String mainUrl = "http://www.joatv.co";
+	public static String subUrl = "http://www.joatv.co";
+	public static boolean isTest = false;
+}
